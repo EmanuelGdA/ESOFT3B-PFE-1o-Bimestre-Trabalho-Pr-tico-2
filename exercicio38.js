bc18583ec02b2868eb8f883f657becd01c5c75f6
JoaoLucasVeiga <joaolucasveigadecarvalho@gmail.com>
2026-04-09 21:41:45 -0300

    Revise comments and maintain contarPalavras function
    Updated header comments to include a descriptive purpose.

--- exercicio38.js
+++ exercicio38.js
@@ -13,7 +13,7 @@ Componentes:
              25363242-2 -  Euclides Benedito Modesto Coelho Neto
              25357453-2 -  Leonardo De Jesus Sabino Flugel
 Data: 05 de Março de 2026
-Descritivo: 
+Descritivo: Dada uma frase, conte quantas palavras ela possui (use `split`).
 ***************************/
 
 
